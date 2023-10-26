b4e8ce2871cff228e4031e4a7dba5fc86475caae
fitri dwi <fitridwirahma11@gmail.com>
2023-10-26 19:49:25 +0700

    add http server

--- server/index.js
+++ server/index.js
@@ -1,1 +1,59 @@
-console.log("Implement servermu disini yak 😝!");
+const http = require("http");
+const { PORT = 8000 } = process.env;
+const url = require("url");
+const fs = require("fs");
+const path = require("path");
+const PUBLIC_DIRECTORY = path.join(__dirname, "../public");
+
+function onRequest(req, res) {
+  res.setHeader("Content-Type", "text/html");
+  switch (req.url) {
+    case "/" || "":
+      req.url = "/index.html";
+      break;
+    case "/cars":
+      req.url = "/cari_mobil.html";
+      break;
+    default:
+      req.url = req.url;
+      break;
+  }
+  const parseURL = url.parse(req.url);
+  const pathName = `${parseURL.pathname}`;
+  const extension = path.parse(pathName).ext;
+  const absolutePath = path.join(PUBLIC_DIRECTORY, pathName);
+  console.log(pathName, extension);
+  console.log(`absolute`, absolutePath);
+  const mapContent = {
+    ".css": "text/css",
+    ".jpg": "image/jpeg",
+    ".html": "text/html",
+    ".js": "text/javascript",
+    ".svg": "image/svg+xml",
+  };
+
+  fs.exists(absolutePath, (exist) => {
+    if (!exist) {
+      res.writeHead(404);
+      res.end("FILE NOT FOUND");
+      return;
+    }
+  });
+
+  fs.readFile(absolutePath, (err, data) => {
+    if (err) {
+      res.statusCode = 500;
+      res.end("FILE NOT FOUND");
+      console.log(err);
+    } else {
+      res.setHeader("Content-Type", mapContent[extension] || "text/plain");
+      res.end(data);
+    }
+  });
+}
+
+const server = http.createServer(onRequest);
+
+server.listen(PORT, "0.0.0.0", () => {
+  console.log("Server sudah berjalan, silahkan buka http://0.0.0.0:%d", PORT);
+});
