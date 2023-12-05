9cb83ce52076d19fd888a7ff2bb41bb991851059
fitri dwi <fitridwirahma11@gmail.com>
2023-12-05 18:24:03 +0700

    seeders

--- seeds/seed_cars.ts
+++ seeds/seed_cars.ts
@@ -6,7 +6,7 @@ export async function seed(knex: Knex): Promise<void> {
 
     // Inserts seed entries
     await knex("cars").insert([
-        { name: 'Car1', photo: 'car1.jpg', price: 50000, sizes_id: 1, created_by: 'superadmin', updated_by: 'superadmin' },
-        { name: 'Car2', photo: 'car2.jpg', price: 60000, sizes_id: 2, created_by: 'superadmin', updated_by: 'superadmin' },
+        { name: 'Car1', photo: 'car1.jpg', price: 50000, sizes_id: 1},
+        { name: 'Car2', photo: 'car2.jpg', price: 60000, sizes_id: 2}
     ]);
 };
